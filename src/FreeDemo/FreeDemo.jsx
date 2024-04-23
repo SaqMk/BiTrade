45b46a28dc6a368566151483122dd75dd13515cf
SaqMk <saq17.mkrtchyan@gmail.com>
2024-04-23 10:31:19 +0400

    add freedemo header

--- src/FreeDemo/FreeDemo.jsx
+++ src/FreeDemo/FreeDemo.jsx
@@ -1,13 +1,12 @@
-import React from 'react'
-import HeaderTraiiding from './HeaderTraiding/HeaderTraiiding'
-import styles from '../FreeDemo/freeDemo.module.css'
+import React from "react";
+import HeaderTraiiding from "./HeaderTraiding/HeaderTraiiding";
+import styles from "../FreeDemo/freeDemo.module.css";
 export default function FreeDemo() {
   return (
     <div className={styles.container}>
-        <div className={styles.wrapper}>
-
-        <HeaderTraiiding/>
-        </div>
+      <div className={styles.wrapper}>
+        <HeaderTraiiding />
+      </div>
     </div>
-  )
+  );
 }
--- src/FreeDemo/HeaderTraiding/HeaderTraiiding.jsx
+++ src/FreeDemo/HeaderTraiding/HeaderTraiiding.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
-import styles from '../HeaderTraiding/headerTraiding.module.css'
+import React from "react";
+import styles from "../HeaderTraiding/headerTraiding.module.css";
+import HeaderIcon from "../../images/HeaderIcon.svg";
+import bmenu from "../../images/bmenu.svg";
+import accImg from "../../images/accImg.svg";
+import { Link } from "react-router-dom";
+
 export default function HeaderTrauiding() {
   return (
     <div className={styles.container}>
-      <div className={styles.wrapper}></div>
+      <div className={styles.wrapper}>
+        <div className={styles.iconContainer}>
+            <img className={styles.bmenu} src={bmenu} />
+          <Link to="/">
+            <img className={styles.HeaderIcon} src={HeaderIcon} />
+          </Link>
+        </div>
+        <div className={styles.accContainer}>
+          <img className={accImg} src={accImg} />
+          <p className={styles.moneyAcc}>$10000</p>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
