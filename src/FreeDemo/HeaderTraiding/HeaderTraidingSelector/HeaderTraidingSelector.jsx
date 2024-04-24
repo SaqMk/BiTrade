956145e57504a3cf6cba418ee1a4f0bd7bc3ee53
SaqMk <saq17.mkrtchyan@gmail.com>
2024-04-24 17:36:05 +0400

    add reg

--- src/FreeDemo/HeaderTraiding/HeaderTraidingSelector/HeaderTraidingSelector.jsx
+++ src/FreeDemo/HeaderTraiding/HeaderTraidingSelector/HeaderTraidingSelector.jsx
@@ -1,84 +1,91 @@
 import styles from "../HeaderTraidingSelector/headerTraidingSelector.module.css";
 import traiding from "../../../images/traiding.svg";
-import historycDemo from "../../../images/historycDemo.svg";
-import toptrDemo from "../../../images/toptrDemo.svg";
-import schoolDemo from "../../../images/schoolDemo.svg";
-import balanceDemo from "../../../images/balanceDemo.svg";
-import accountCircleDemo from "../../../images/accountCircleDemo.svg";
-import chatDemo from "../../../images/chatDemo.svg";
+import historyDemo from "../../../images/historyDemo1.svg";
+import toptrDemo from "../../../images/toptrDemo1.svg";
+import schoolDemo from "../../../images/schoolDemo1.svg";
+import balanceDemo from "../../../images/balanceDemo1.svg";
+import accountCircleDemo from "../../../images/accountCircleDemo1.svg";
+import chatDemo from "../../../images/chatDemo1.svg";
 import logout from "../../../images/logout.svg";
 import { Link } from "react-router-dom";
 import { useState, useEffect } from "react";
-function HorizontalPanel({ panelVisible }) {
 
+function HorizontalPanel({ panelVisible }) {
   const [state, setState] = useState([
     {
-      id:1,
-      img: traiding,
-    },
+       id: 1, img: traiding, 
+       title:'Traiding'
+      },
     {
-      id:2,
-
-      img: historycDemo,
+       id: 2, img: historyDemo ,
+       title:'History'
+      },
+    
+      { 
+        id: 3, img: toptrDemo ,
+        title:'Top traders'
+      },
+    { 
+      id: 4, img: schoolDemo ,
+      title:'Education'
     },
+    { 
+      id: 5, img: balanceDemo ,
+      title:'Wallet'
+    },
+    { 
+      id: 6, img: accountCircleDemo,
+      title:'Profile'
+     },
     {
-      id:3,
+       id: 7, img: chatDemo ,
+       title:'Chat'
+      },
+  ]);
 
-      img: toptrDemo,
-    },
-    {
-      id:4,
+  const [activeIndex, setActiveIndex] = useState(0);
 
-      img: schoolDemo,
-    },
-    {
-      id:5,
-
-      img: balanceDemo,
-    },
-    {
-      id:6,
-
-      img: accountCircleDemo,
-    },
-    {
-      id:7,
-
-      img: chatDemo,
-    },
-  ])
-
-  function handleClick(ind){
-    console.log(ind)
+  const handleClick = (ind) => {
+    setActiveIndex(ind);
   };
   const [browserHeight, setBrowserHeight] = useState(window.innerHeight);
-
   useEffect(() => {
     function handleResize() {
       setBrowserHeight(window.innerHeight);
     }
-    window.addEventListener('resize', handleResize);
+    window.addEventListener("resize", handleResize);
     return () => {
-      window.removeEventListener('resize', handleResize);
+      window.removeEventListener("resize", handleResize);
     };
   }, []);
 
-
-  console.log(browserHeight)
   return (
-    <div  className={styles.horizontalPanel}>
-      <div style={{ height: `calc(${browserHeight}px - 5vw)` }} className={` ${styles.panel} ${panelVisible ? styles.visible : ""}`}>
+    <div className={styles.horizontalPanel}>
+      <div
+        style={{ height: `calc(${browserHeight}px - 5vw)` }}
+        className={` ${styles.panel} ${panelVisible ? styles.visible : ""}`}
+      >
         <div className={styles.wrapper}>
-          {state.map((item,ind) => (
-            <div key={ind} onClick={handleClick(ind)} className={styles.contentContainer}>
-              <img className={styles.itemImg} src={item.img} />
+          {state.map((item, ind) => (
+            <div
+              key={ind}
+              onClick={() => handleClick(ind)}
+              className={`${styles.contentContainer} ${
+                activeIndex === ind ? styles.active : ""
+              }`}
+            >
+              <img
+                className={styles.itemImg}
+                src={item.img}
+              />
+              <p>{item.title}</p>
             </div>
           ))}
         </div>
-        <Link to = "/">
-        <div className={styles.contentContainer}>
-          <img className={styles.itemImg} src={logout} />
-        </div>
+        <Link to="/register">
+          <div className={styles.contentContainer}>
+            <img className={styles.itemImg} src={logout} alt="Logout" />
+          </div>
         </Link>
       </div>
     </div>
--- src/Register/Register.jsx
+++ src/Register/Register.jsx
@@ -1,19 +1,36 @@
-import React from 'react'
-import Header from '../HeaderFooter/Header'
-import styles from '../Register/register.module.css'
-import Footer from '../HeaderFooter/Footer'
-import rafiki from '../images/rafiki.svg'
+import React from "react";
+import Header from "../HeaderFooter/Header";
+import styles from "../Register/register.module.css";
+import Footer from "../HeaderFooter/Footer";
+import rafiki from "../images/rafiki.svg";
 export default function Register() {
   return (
     <>
-        <Header/>
-        <div className={styles.container}>
-            <div className={styles.wrapper}>
-                <div className={styles.formDiv}></div>
-                <img className={styles.rafiki} src={rafiki}/>
+      <Header />
+      <div className={styles.container}>
+        <div className={styles.wrapper}>
+          <div className={styles.formDiv}>
+            <p className={styles.title}>Welcome to BItrade!</p>
+            <p className={styles.createAcc}>Create an account for free</p>
+            <div className={styles.inputContainer}>
+              <div className={styles.formWrapper}>
+                <label className={styles.label}>Your email*</label>
+                <input className={styles.formInput} type="text" />
+              </div>
+              <div className={styles.formWrapper}>
+                <label className={styles.label}>Your email*</label>
+                <input className={styles.formInput} type="text" />
+              </div>
+              <div className={styles.formWrapper}>
+                <label className={styles.label}>Your email*</label>
+                <input className={styles.formInput} type="text" />
+              </div>
             </div>
+          </div>
+          <img className={styles.rafiki} src={rafiki} />
         </div>
-        <Footer/>
+      </div>
+      <Footer />
     </>
-  )
+  );
 }
